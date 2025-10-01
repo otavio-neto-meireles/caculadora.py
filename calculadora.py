202bfb33f8de1f0af8f576233dbc00018152cec1
Otávio-Neto-Meireles <otavio.meireles@prototipandoaquebrada.org>
2025-10-01 15:17:31 -0300

    first commit

---
+++ calculadora.py
@@ -0,0 +1,21 @@
+# Calculadora 
+
+print (f"se atente-se que é uma caculadora seu cabaço, então coloca só numero e arrombado!")
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+operacao = input("Digite a operação: ")
+
+match operacao:
+    case "+":
+        res = num1 + num2
+    case "-":
+        res = num1 - num2
+    case "*":
+        res = num1 * num2
+    case "/":
+        res = num1 / num2
+ 
+ 
+
+
+print(f"Resultado é igual a {res}")
