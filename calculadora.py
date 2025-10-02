347f948520ccea7b109691ad3ff001e93d6563f7
Otávio-Neto-Meireles <otavio.meireles@prototipandoaquebrada.org>
2025-10-02 09:29:26 -0300

    Primeiros passos

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,6 @@
 # Calculadora 
 
-print (f"se atente-se que é uma caculadora seu cabaço, então coloca só numero e arrombado!")
+print (f"se atente-se que é uma caculadora seu cabaço, então coloca só numero o arrombado!")
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 operacao = input("Digite a operação: ")
@@ -15,9 +15,9 @@ match operacao:
     case "/":
         res = num1 / num2
 print(f"continuar caculando?")
-    
+
 
  
 
 
-print(f"Resultado é igual a {res}")
+print(f"Resultado é igual a {res}")  
